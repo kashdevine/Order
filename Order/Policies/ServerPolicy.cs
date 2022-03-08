e2c15423d878958ce8cfe588a3dac2dbc26a6946
kashdevine <kasheen.mowatt@gmail.com>
2022-03-07 22:46:56 -0800

    Created and implemented a couple policies to handle failures connecting to the db.

---
+++ Order/Policies/ServerPolicy.cs
@@ -0,0 +1,33 @@
+﻿using Polly;
+using Polly.Retry;
+using System.Data.Entity.Core;
+
+namespace Order.Policies
+{
+    public class ServerPolicy
+    {
+        public AsyncRetryPolicy RetryForever { get; }
+        public AsyncRetryPolicy RetryLimited { get; }
+
+        public ServerPolicy()
+        {
+            RetryLimited = Policy.Handle<EntityException>().RetryAsync
+                (
+                    5,
+                    onRetry: (exception, retryCount) =>
+                        {
+                            Console.Error.WriteLine(String.Format("{0}: failed to connect to db {1} time(s). Retrying...", exception, retryCount));
+                        }
+                );
+
+            RetryForever = Policy.Handle<EntityException>().WaitAndRetryForeverAsync
+                (
+                retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)),
+                onRetry: (exception, retryCount) =>
+                    {
+                        Console.Error.WriteLine(String.Format("{0}: failed to connect to db {1} time(s). Retrying...", exception, retryCount));
+                    }
+                );
+        }
+    }
+}
