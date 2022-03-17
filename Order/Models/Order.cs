97928c268d49a736e2c0d5ab61a2f2c423c7768c
kashdevine <kasheen.mowatt@gmail.com>
2022-03-17 10:31:01 -0700

    Created the billing address class and added name to the order class.

--- Order/Models/Order.cs
+++ Order/Models/Order.cs
@@ -5,7 +5,9 @@ namespace Order.Models
     public class Order
     {
         public Guid Id { get; set; }
+        public string? Name { get; set; }
         public Guid BillingAddressId { get; set; }
+        public BillingAddress BillingAddress { get; set; }
         public Guid ShippingAddressId { get; set; }
         public Dictionary<Guid,int>? Items { get; set; }
         public PaymentStatusEnum PaymentStatus { get; set; }
